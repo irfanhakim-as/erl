727c98c27050d60947150bf304b07ca5f7e6312c
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-05-13 01:19:02 +0800

    Reorder flag

--- installer.sh
+++ installer.sh
@@ -9,21 +9,18 @@ source "./share/md.sh"
 function print_help() {
     echo "Usage: ${0} [options]"; echo
     echo "OPTIONS:"
-    echo "  -h, --help                           Print help message"
-    # echo "  -c, --config-prefix <prefix>         Specify a config prefix"
-    echo "  -i, --install-prefix <prefix>        Specify an installation prefix"
+    # echo "  -c, --config-prefix <path>           Specify a config prefix"
+    echo "  -i, --install-prefix <path>          Specify an installation prefix"
     echo "  -l, --link-install                   Perform a symlink installation"
     echo "  -u, --uninstall                      Uninstall application"
+    echo "  -h, --help                           Print help message"; echo
+    echo "Report bugs to ${__source__}/issues"
 }
 
 
 # get optional arguments
 while [[ ${#} -gt 0 ]]; do
     case "${1}" in
-        -h|--help)
-            print_help
-            exit 0
-            ;;
         # -c|--config-prefix)
         #     if [ -z "${2}" ]; then
         #         echo "ERROR: Please specify a config prefix"
@@ -45,6 +42,10 @@ while [[ ${#} -gt 0 ]]; do
             ;;
         -u|--uninstall)
             UNINSTALL_APP=1
+            ;;
+        -h|--help)
+            print_help
+            exit 0
             ;;
         *)
             echo "ERROR: Invalid argument (${1})"
