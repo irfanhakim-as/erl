8fddd0703080ae8d01239c354c44b654d0d8a3f3
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-05-13 01:17:45 +0800

    Add project metadata

---
+++ share/md.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+#         :::   :::  ::::::::::::::    :::    :::
+#       :+:+: :+:+:     :+:    :+:   :+:   :+: :+:
+#     +:+ +:+:+ +:+    +:+    +:+  +:+   +:+   +:+  Irfan Hakim (MIKA)
+#    +#+  +:+  +#+    +#+    +#++:++   +#++:++#++:  https://sakurajima.social/@irfan
+#   +#+       +#+    +#+    +#+  +#+  +#+     +#+   https://github.com/irfanhakim-as
+#  #+#       #+#    #+#    #+#   #+# #+#     #+#    https://gitlab.com/irfanhakim
+# ###       #################    ######     ###
+#
+# erl-metadata: Metadata for the Easy Relative Linking tool.
+
+
+__name__="erl"
+__namespace__="erl"
+__version__="0.1.0"
+__description__="Easy Relative Linking tool written in Bash."
+__author__="Irfan Hakim"
+__author_email__="irfanhakim.as@yahoo.com"
+__author_url__="https://github.com/irfanhakim-as"
+__source__="https://github.com/irfanhakim-as/${__namespace__}"
+__license__=""
